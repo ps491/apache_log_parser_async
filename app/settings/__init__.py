3d4f19c115cb2cf9c9f75f1545fc73992c557b28
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-05-25 21:27:14 +0500

    refactoring settings

--- app/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from decouple import AutoConfig
-
-BASE_DIR = Path(__file__).parent.parent
-print(BASE_DIR)
-config = AutoConfig(search_path=BASE_DIR.joinpath('config'))
---
+++ app/settings/config.py
@@ -0,0 +1,17 @@
+from environs import Env
+
+DEBUG = True
+
+if DEBUG:
+    env = Env()
+    env.read_env(".env.dev", recurse=False)
+else:
+    env = Env()
+    env.read_env()
+
+POSTGRES_HOST = env.str("POSTGRES_HOST")
+POSTGRES_PORT = env.str("POSTGRES_PORT")
+USER = env.str("USER")
+PASSWORD = env.str("PASSWORD")
+DB_NAME = env.str("DB_NAME")
+
